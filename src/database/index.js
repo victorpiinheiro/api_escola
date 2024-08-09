c9b653a917c133cd898b8118fc5239d27f213db4
Victor Pinheiro <contato.vspinheiro@gmail.com>
2024-08-09 11:56:58 -0300

    colocando fotos no banco de dados

--- src/database/index.js
+++ src/database/index.js
@@ -4,10 +4,12 @@ import databaseConfig from '../config/database';
 
 import Aluno from '../models/Aluno';
 import User from '../models/User';
+import Foto from '../models/Foto';
 
 const connection = new Sequelize(databaseConfig);
-const models = [Aluno, User];
+const models = [Aluno, User, Foto];
 
 models.forEach((model) => model.init(connection));
+models.forEach((model) => model.associate && model.associate(connection.models));
 
 export default connection;
---
+++ src/models/Foto.js
@@ -0,0 +1,35 @@
+import Sequelize, { Model } from 'sequelize';
+
+export default class Aluno extends Model {
+  static init(sequelize) {
+    super.init({
+      originalname: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Campo nao pode ficar vazio',
+          },
+        },
+      },
+      filename: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          notEmpty: {
+            msg: 'Campo nao pode ficar vazio',
+          },
+        },
+      },
+
+    }, {
+      sequelize,
+      tableName: 'fotos',
+    });
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Aluno, { foreignKey: 'aluno_id' });
+  }
+}
